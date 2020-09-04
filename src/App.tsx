aa09458e4701653d2067ab61e7249cf85e4acf9b
Chantawat Yangtrong <cyredhat@gmail.com>
2020-09-04 13:36:12 +0700

    add top bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { CSSReset } from '@chakra-ui/core'
 import Providers from 'providers'
 import './App.css'
+import './components/TopBar'
+import TopBar from './components/TopBar'
 
 function App() {
   return (
     <Providers>
       <CSSReset />
-      {/*insert children here*/}
+      <TopBar />
     </Providers>
   )
 }
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Flex, Image } from '@chakra-ui/core'
+
+const TopBar = () => {
+  return (
+    <Flex
+      bg="white"
+      w="100%"
+      h="75px"
+      alignItems="center"
+      boxShadow="0px 1px 1px rgba(0, 0, 0, 0.15)"
+    >
+      <Image src="/images/esc-logo.png" ml={[0, 0, '16.67%']}></Image>
+    </Flex>
+  )
+}
+
+export default TopBar
