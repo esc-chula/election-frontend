293eb09e998e8c1ab42c7b269c6019e6ce52fda3
Nut Pinyo <bomb.np@gmail.com>
2020-09-04 17:34:42 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
-import { CSSReset, Box } from '@chakra-ui/core'
+import { CSSReset } from '@chakra-ui/core'
 import Providers from 'providers'
 import './App.css'
 import './components/TopBar'
 import TopBar from './components/TopBar'
 import Footer from './components/Footer'
 import PageWrapper from './components/PageWrapper'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import IndexPage from 'pages'
 
 function App() {
   return (
-    <Providers>
-      <CSSReset />
-      <TopBar />
-      <PageWrapper>
-        <Box />
-        <Footer />
-      </PageWrapper>
-    </Providers>
+    <Router>
+      <Providers>
+        <CSSReset />
+        <TopBar />
+        <PageWrapper>
+          <Switch>
+            <Route path="/" component={IndexPage} />
+          </Switch>
+          <Footer />
+        </PageWrapper>
+      </Providers>
+    </Router>
   )
 }
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Box } from '@chakra-ui/core'
+
+const IndexPage = () => {
+  return <Box />
+}
+
+export default IndexPage
