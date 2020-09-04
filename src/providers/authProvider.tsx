a1e7357570e8db788aecc240e8853acebadcb606
Nut Pinyo <bomb.np@gmail.com>
2020-09-04 16:27:05 +0700

    finish AuthProvider

--- src/providers/authProvider.tsx
+++ src/providers/authProvider.tsx
@@ -1,7 +1,18 @@
-import React, { useContext, createContext, useState } from 'react'
+import React, { useContext, createContext, useState, useCallback } from 'react'
+import axios from 'axios'
+import { API_HOST } from 'config/env'
+import { ExchangeTokenDTO } from 'types/dto'
+import { handleAxiosError } from 'config/util'
+
+export interface AuthUser {
+  username: string
+}
 
 export interface AuthConstruct {
   token: string | null
+  authUser: AuthUser | null
+  logout: () => void
+  exchangeTicket: (token: string) => void
 }
 
 export const AuthContext = createContext({} as AuthConstruct)
@@ -13,8 +24,30 @@ interface AuthProviderProps {
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
   const [token, setToken] = useState<string | null>(null)
+  const [authUser, setAuthUser] = useState<AuthUser | null>(null)
+
+  const logout = useCallback(() => {
+    setToken(null)
+    setAuthUser(null)
+  }, [])
+
+  const exchangeTicket = useCallback(async (token: string) => {
+    try {
+      const data: ExchangeTokenDTO = await axios.post(
+        `${API_HOST}/auth/exchangetoken?token=${token}`,
+      )
+      setToken(data.jwt)
+      setAuthUser({ username: data.user.username } as AuthUser)
+    } catch (e) {
+      handleAxiosError(e)
+    }
+  }, [])
+
   const value = {
     token,
+    authUser,
+    logout,
+    exchangeTicket,
   }
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
