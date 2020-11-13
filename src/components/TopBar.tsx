6c2c6b52c233c6a27d33ff077990cb296efe53c9
Uttanon <uttanon.aug@gmail.com>
2020-11-13 14:07:05 +0700

    add menu bar in navbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import { Flex, Image } from '@chakra-ui/core'
+import {
+  Button,
+  Flex,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Icon,
+} from '@chakra-ui/core'
 
 import escLogo from '../images/esc-logo.svg'
 
@@ -13,8 +22,26 @@ const TopBar = () => {
       boxShadow="0px 1px 1px rgba(0, 0, 0, 0.15)"
       top="0"
       position="fixed"
+      justifyContent="space-between"
     >
       <Image src={escLogo} ml={[0, '16.67%']}></Image>
+      <Menu>
+        <MenuButton
+          as={Button}
+          color="intaniaRed.600"
+          backgroundColor="white"
+          mr={[0, '16.67%']}
+          _hover={{ bg: 'intaniaRed.50' }}
+        >
+          623xxxxx21
+          <Icon name="chevron-down" size="24px"></Icon>
+        </MenuButton>
+        <MenuList>
+          <MenuItem backgroundColor="white" _hover={{ bg: 'white' }}>
+            ออกจากระบบ
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </Flex>
   )
 }
