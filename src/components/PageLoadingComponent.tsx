bc2541da1d63d0dedbc8cf8f897a411848151819
paphonb <paphonb@gmail.com>
2020-11-14 17:49:54 +0700

    Fix loading indicator

--- src/components/PageLoadingComponent.tsx
+++ src/components/PageLoadingComponent.tsx
@@ -2,6 +2,7 @@ import { Flex, Spinner } from '@chakra-ui/react'
 import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 import React from 'react'
+import { useIntaniaRed } from 'util/hooks'
 import { usePageLoadingContext } from './PageLoadingProvider'
 
 const fadeIn = keyframes`
@@ -17,6 +18,12 @@ const fadeIn = keyframes`
 `
 
 const Container = styled(Flex)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
   animation: ${fadeIn} 1s ease;
 `
 
@@ -26,8 +33,8 @@ export default function PageLoadingComponent() {
     return null
   }
   return (
-    <Container flex="1" justifyContent="center" alignItems="center">
-      <Spinner thickness="4px" colorScheme="intaniaRed" size="xl" />
+    <Container justifyContent="center" alignItems="center">
+      <Spinner thickness="4px" color={useIntaniaRed()} size="xl" />
     </Container>
   )
 }
