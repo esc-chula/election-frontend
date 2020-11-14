0bb88bf2c70bbde45729d4776229f7c941854c4a
paphonb <paphonb@gmail.com>
2020-11-14 15:46:23 +0700

    Remove footer

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/react'
-import Footer from './Footer'
 
 interface Props {
   children: React.ReactNode
@@ -8,11 +7,8 @@ interface Props {
 
 const PageWrapper = ({ children }: Props) => {
   return (
-    <Flex minH="100vh" flexDirection="column">
-      <Flex flex={1} flexDirection="column" pt="75px">
-        {children}
-      </Flex>
-      <Footer />
+    <Flex flex={1} flexDirection="column" pt="75px">
+      {children}
     </Flex>
   )
 }
