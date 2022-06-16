278ac6e850a448a2fb270d337e377f638857e61f
PongDev <pongwiwat.dev@outlook.com>
2022-06-16 23:05:57 +0700

    Patch academicYear at src/util/constants.ts to begin new academic year after May

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,2 +1,3 @@
 const thisYear = parseInt(`${new Date().getFullYear() + 543}`.slice(2))
-export const academicYear = new Date().getMonth() < 7 ? thisYear - 1 : thisYear
+// export const academicYear = new Date().getMonth() < 7 ? thisYear - 1 : thisYear
+export const academicYear = new Date().getMonth() < 5 ? thisYear - 1 : thisYear
